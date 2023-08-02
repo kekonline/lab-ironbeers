b99bddca25eabbe34a209763200f74f656abfecf
kekonline <kekonline@gmail.com>
2023-08-02 17:28:31 +0200

    Doing well really fast

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
+
+
 const app = express();
 const punkAPI = new PunkAPIWrapper();
+app.use(express.static("public"))
+
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +23,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+
+
+
+
 });
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then((beersFromApi) => {
+
+
+      // console.log(beersFromApi)
+      res.render('beers.hbs', { beersFromApi });
+
+
+    })
+
+
+    .catch(error => console.log(error));
+
+
+
+
+
+
+});
+
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));

9d9fbb85395555976117e846000f8cb39e627b62
Lucas Navarro <navarrorlucas@gmail.com>
2023-08-02 18:15:33 +0200

    MVP + some bonus

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
-
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-app.use(express.static("public"))
-
+app.use(express.static('public'));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,40 +15,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
+
+hbs.registerPartials(__dirname + "/views/partials")
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index.hbs');
-
-
-
-
 });
 
 app.get('/beers', (req, res) => {
   punkAPI
     .getBeers()
-    .then((beersFromApi) => {
-
-
+    .then(beersFromApi => {
       // console.log(beersFromApi)
       res.render('beers.hbs', { beersFromApi });
-
-
     })
 
-
     .catch(error => console.log(error));
-
-
-
-
-
-
 });
 
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+       console.log("este es"+beersFromApi)
+      res.render('random-beer.hbs', { beersFromApi });
+    })
+
+    .catch(error => console.log(error));
+});
 
 
 
